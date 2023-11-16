dd0f6300f8ab9e558250523b7b63f649079763b2
Ben Wu <wubin28@gmail.com>
2023-11-16 17:03:47 +0800

    feat: print out fibs using a lazy list

---
+++ src/my_code_for_book_functional_design/ch04/fibs_laziness.clj
@@ -0,0 +1,19 @@
+(ns my-code-for-book-functional-design.ch04.fibs-laziness)
+
+(declare fib)
+
+(defn fib-w [n]
+  (cond
+    (< n 1) nil
+    (<= n 2) 1
+    :else (+ (fib (dec n)) (fib (- n 2)))))
+
+(def fib (memoize fib-w))
+
+; Define the lazy-fibs function to generate a lazy sequence of Fibonacci numbers
+(defn lazy-fibs []
+  (map fib (rest (range))))
+
+; Update the -main function to use lazy-fibs and print the first 15 Fibonacci numbers
+(defn -main [& args]
+  (println (take 19 (lazy-fibs))))
