f241d891d049af4fa047ff831d61bf0895a6e97d
Ben Wu <wubin28@gmail.com>
2023-11-16 10:46:07 +0800

    feat: We can vastly improve the performance by using iteration

---
+++ src/my_code_for_book_functional_design/ch03/fibs_recursion_2.clj
@@ -0,0 +1,20 @@
+(ns my-code-for-book-functional-design.ch03.fibs-recursion-2)
+
+(defn ifib
+  ([n a b]
+   (if (= 0 n)
+     b
+     (recur (dec n) b (+ a b))))
+
+  ([n]
+   (cond
+     (< n 1) nil
+     (<= n 2) 1
+     :else (ifib (- n 2) 1 1)))
+  )
+
+(defn fibs [n]
+  (map ifib (range 1 (inc n))))
+
+(defn -main [& args]
+  (println (fibs 16)))
