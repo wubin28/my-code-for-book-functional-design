0c30d00b8a61d15e04ad2251b8486128c4812463
Ben Wu <wubin28@gmail.com>
2023-11-20 09:13:06 +0800

    feat: There are several ways to use STM in Clojure, but the simplest is the atom. An atom is an atomic value that can be altered using the swap! function.

---
+++ src/my_code_for_book_functional_design/ch05/stm.clj
@@ -0,0 +1,20 @@
+(ns my-code-for-book-functional-design.ch05.stm)
+
+(def counter (atom 0))
+
+(defn add-one [x]
+  (let [y (inc x)]
+    (print (str "(" x ")"))
+    y))
+
+(defn increment [n id]
+  (dotimes [_ n]
+    (print id)
+    (swap! counter add-one)))
+
+(defn -main []
+  (let [ta (future (increment 10 "a"))
+        tx (future (increment 10 "x"))
+        _ @ta
+        _ @tx]
+    (println "\nCounter is: " @counter)))
